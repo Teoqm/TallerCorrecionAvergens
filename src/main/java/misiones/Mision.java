894e8f03bd82be40c99d764f6b4a45562c0573e8
Juan Jose Morales Aristizabal <juanj.moralesa@autonoma.edu.co>
2026-03-07 02:38:29 -0500

    feat: implementa gestor de misiones y asignacion automatica

--- src/main/java/misiones/Mision.java
+++ src/main/java/misiones/Mision.java
@@ -3,21 +3,15 @@ package misiones;
 public class Mision {
 
     private String nombre;
-    private String descripcion;
     private TipoHabilidad habilidadRequerida;
 
-    public Mision(String nombre, String descripcion, TipoHabilidad habilidadRequerida) {
+    public Mision(String nombre, TipoHabilidad habilidadRequerida) {
         this.nombre = nombre;
-        this.descripcion = descripcion;
         this.habilidadRequerida = habilidadRequerida;
     }
 
     public String getNombre() {
         return nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
     }
 
     public TipoHabilidad getHabilidadRequerida() {
@@ -27,7 +21,6 @@ public class Mision {
     @Override
     public String toString() {
         return "Mision: " + nombre +
-                "\nDescripcion: " + descripcion +
                 "\nHabilidad requerida: " + habilidadRequerida;
     }
 }
